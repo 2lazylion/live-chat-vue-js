45aa87df93ac95982d934ab702a17c5c1ab72cc2
2lazylion <e_cadet22@hotmail.com>
2021-04-04 12:31:31 -0400

    git init

---
+++ src/components/Signupform.vue
@@ -0,0 +1,17 @@
+<template>
+  <form>
+      <input type="text" required placeholder="display name" v-model="displayName">
+      <input type="text" required placeholder="email" v-model="email">
+      <input type="password" >
+  </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
