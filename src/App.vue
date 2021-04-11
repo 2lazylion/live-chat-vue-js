5409cc159a854ba474006c711c822315166c646e
2lazylion <e_cadet22@hotmail.com>
2021-04-11 15:56:43 -0400

    footer added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <template>
   
   <router-view/>
+  <Signature />
 </template>
+
+<script>
+import Signature from './components/Signature.vue'
+
+export default {
+  components: { Signature },
+  setup() {
+    
+  }
+}
+</script>
 
 <style>
 #app {
---
+++ src/components/Signature.vue
@@ -0,0 +1,44 @@
+<template>
+  <footer>
+    <p>Made by Edward Cadet with Net Ninja's Udemy Course</p>
+    <p>
+      github:
+      <a href="https://github.com/2lazylion/live-chat-vue-js">
+        https://github.com/2lazylion/live-chat-vue-js
+        </a>
+    </p>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+footer {
+  display: flexbox;
+  justify-content: center;
+  margin-bottom: 20px;
+  text-align: center;
+}
+
+footer p {
+    margin: 10px 0;
+}
+
+footer a {
+    color: #49a393;
+}
+
+footer a:visited {
+    color: #32685e;
+}
+
+footer a:hover {
+    color: #5ae4ca;
+}
+
+footer a:active {
+    color: #32685e;
+}
+</style>
